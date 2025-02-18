8d343811e47bbc3fed42785c167c78d08844348d
cameron <cuckmeron@gmail.com>
2025-02-17 23:08:20 -0500

    .

--- src/main/java/com/memeasaur/cubecorelaby/CubecoreLaby.java
+++ src/main/java/com/memeasaur/cubecorelaby/CubecoreLaby.java
@@ -1,17 +1,37 @@
 package com.memeasaur.cubecorelaby;
 
+import net.kyori.adventure.text.Component;
+import net.labymod.serverapi.core.model.moderation.InstalledAddon;
+import net.labymod.serverapi.server.bukkit.event.LabyModPlayerJoinEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 
-public final class CubecoreLaby extends JavaPlugin {
+public final class CubecoreLaby extends JavaPlugin implements Listener {
     public static ArrayList<String> requestList = new ArrayList<>();
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new CubecoreLabyListeners(), this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
+    }
+
+    @EventHandler
+    void onLabyJoin(LabyModPlayerJoinEvent e) {
+        e.labyModPlayer().requestInstalledAddons(requestList, response -> {
+            for (InstalledAddon addon : response.getInstalledAddons()) {
+                if (addon.isLocal()) {
+                    e.labyModPlayer().getPlayer().kick(Component.text("kicked (non-store addon)"));
+                }
+                else if (addon.getNamespace().equals("labyfabric")) {
+                    e.labyModPlayer().getPlayer().sendMessage("labyfabric isn't allowed once the optifine addon is available");
+                }
+                e.labyModPlayer().disableAddons("damageindicator");
+            }
+        });
     }
 }
