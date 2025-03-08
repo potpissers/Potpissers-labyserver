12f0d38d89a81e2ecf7363f5f50de9731232f1bf
cameron <cuckmeron@gmail.com>
2025-03-08 01:41:49 -0500

    .

--- src/main/java/com/memeasaur/cubecorelaby/CubecoreLaby.java
+++ src/main/java/com/memeasaur/cubecorelaby/CubecoreLaby.java
@@ -2,17 +2,19 @@ package com.memeasaur.cubecorelaby;
 
 import net.kyori.adventure.text.Component;
 import net.labymod.serverapi.core.model.moderation.InstalledAddon;
+import net.labymod.serverapi.server.bukkit.LabyModProtocolService;
 import net.labymod.serverapi.server.bukkit.event.LabyModPlayerJoinEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public final class CubecoreLaby extends JavaPlugin implements Listener {
-    public static ArrayList<String> requestList = new ArrayList<>();
+    public static List<String> REQUEST_LIST = List.of();
     @Override
     public void onEnable() {
+        LabyModProtocolService.initialize(this);
         getServer().getPluginManager().registerEvents(this, this);
     }
 //    @Override
@@ -20,7 +22,7 @@ public final class CubecoreLaby extends JavaPlugin implements Listener {
 //    }
     @EventHandler
     void onLabyJoin(LabyModPlayerJoinEvent e) {
-        e.labyModPlayer().requestInstalledAddons(requestList, response -> {
+        e.labyModPlayer().requestInstalledAddons(REQUEST_LIST, response -> {
             for (InstalledAddon addon : response.getInstalledAddons()) {
                 if (addon.isLocal()) {
                     e.labyModPlayer().getPlayer().kick(Component.text("kicked (non-store addon)"));
